6f2fe04c5dc400c7419c190c70f30822244dd1e2
Carlos Arbizu <carlos2345532@gmail.com>
2022-03-06 19:55:55 -0600

    Finished login page

--- js-file.js
+++ js-file.js
@@ -1,6 +1,19 @@
-const password = document.querySelector("password");
-const passwordConfirm = document.querySelector("passworConfirm");
+const password = document.querySelector("#password");
+const passwordConfirm = document.querySelector("#passwordConfirm");
+let smallText = document.createElement("small");
+smallText.textContent = "* Passwords do not match";
+smallText.style.cssText = "font-size: 10px; color: red; text-shadow: 2px black;"
+const check = function(e) {
+    if(password.value !== passwordConfirm.value){
+        password.after(smallText);
+        password.setCustomValidity("Invalid field");
+        passwordConfirm.setCustomValidity("Invalid field");
+    }
+    else {
+        smallText.remove();
+        password.setCustomValidity("");
+        passwordConfirm.setCustomValidity("");
+    }
+}   
 
-const check = function() {
-
-}
+password.addEventListener(onkeyup,check);
